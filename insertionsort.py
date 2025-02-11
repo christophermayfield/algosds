d20d1ec03475431fa6e343d2158117d28b8d88eb
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-02-10 19:22:06 -0800

    update function

--- insertionsort.py
+++ insertionsort.py
@@ -5,7 +5,7 @@ def insertion_sort(arr):
                 j = i - 1
                 while j >= 0 and arr[j] > key:
                         arr[j+1] = arr[j]
-                        j=j-1
+                        j-=1
                 arr[j+1] = key 
         return arr
 
