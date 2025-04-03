cb73af4fa21c5798c35bd7f6197fbf906cb057f2
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-03 14:12:01 -0700

    add code

--- countingsort.py
+++ countingsort.py
@@ -16,3 +16,9 @@ def counting_sort(arr):
     for num, freq in enumerate(count):
         sorted_arr.extend([num] * freq)
     return sorted_arr
+
+
+
+arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(arr)
+print("Sorted array:", sorted_arr)
