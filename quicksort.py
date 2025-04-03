1708b46c9896cb544bdd3e90b6162f86bd04cc4c
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-03 09:58:40 -0700

    add quick sort

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,7 @@
-#quick sort 
+#quick sort, O(nlogn) average, O(n^2) worst case
+#pick a pivot element, partition the array such that elements < pivot go left
+#elements > pivot go right 
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr 
