fcd945229bc26358a605f99d0ad6fe95983bbfec
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-06 12:15:19 -0700

    add code for house robber problem

--- countingsort.py
+++ countingsort.py
@@ -15,6 +15,7 @@ def counting_sort(arr):
     count = [0] * (max_val + 1)
     for num in arr:
         count[num] += 1
+    print(count)
     
     # Step 3: Modify the count array to store positions
     for i in range(1, len(count)):
---
+++ houserobber.py
@@ -0,0 +1,22 @@
+#3 scenarios 
+#1 - rob only the first house
+#2 - skip the first house, rob the second, repeat
+#3 - rob the first house, skip the second, repeat
+
+def rob(houses):
+    return max(houses[0], slide(houses[1:]), slide(houses[:-1]))
+
+def slide(houses):
+    prev_max = 0
+    best_max = 0
+
+    for house in houses:
+        total = max(prev_max + house, best_max)
+        prev_max = best_max
+        best_max = total
+        
+    return best_max
+
+houses = [2,3,5,1,3]
+
+print(f"the most money we can make is {rob(houses)}")
