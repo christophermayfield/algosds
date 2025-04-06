1104d302cb358684482833569607a451988bb227
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-06 15:35:09 -0700

    update counting sort exp function

--- radixsort.py
+++ radixsort.py
@@ -2,4 +2,14 @@ def radix_sort(arr):
     #step 1 - Find the max number to know number of digits 
     if not arr:
         return arr
-     
+    max_val = max(arr)
+    exp = 1 #we will start with the 1s place 
+
+    #step 2 - loop until we have processed all digit places 
+    while max_val // exp > 0:
+        counting_sort_exp(arr, exp)
+        exp*=10
+    return arr 
+
+def counting_sort_exp(arr,exp):
+    pass 
