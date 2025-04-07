c81bffee4b2c65812d0ade8e4a36ecce4bb8c3bd
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-06 18:51:07 -0700

    update bucket sort

--- radixsort.py
+++ radixsort.py
@@ -23,4 +23,31 @@ def counting_sort_exp(arr,exp):
         count[index]+=1
 
     #step 2 make counts cumulative (so we know the positions)
+    for i in range(1,10):
+        count[i] += count[i-1]
+
     
+    #step 3: build output array (go in reverse for stability)
+    for i in range(n-1,-1,-1):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    if not arr:
+        return arr
+    max_val = max(arr)
+    exp = 1
+
+    while max_val // exp > 0:
+        counting_sort_exp(arr,exp)
+        exp*=10
+    return arr
+
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr)
+print('sorted arr', arr)
