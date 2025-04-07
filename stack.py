28241c6b1fcb01018ed5304bb0917bc411de94c3
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-07 10:05:31 -0700

    update stack.py

--- stack.py
+++ stack.py
@@ -15,6 +15,24 @@ class Stack:
             raise  IndexError("Pop from an empty stack")
         item = self.items.pop()
         print(f'Popped {item} from the stack')
-        
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from an empty stack")
+        return self.items[-1]
+    def size(self):
+        return len(self.items)
+    def __str__(self):
+        return f"stack: {self.items}"
     
     
+    
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    print(stack)
+    print(f"Top item is: {stack.peek()}")
+    stack.pop()
+    print(stack)
+    print(f"Is stack empty? {stack.is_empty()}")
