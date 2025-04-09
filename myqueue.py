e11540a649e491b96d40c9927a136a17cfb01065
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-09 13:41:00 -0700

    add peak and is empty function

--- myqueue.py
+++ myqueue.py
@@ -2,4 +2,26 @@
 
 class Queue:
     def __init__(self):
-        pass
+        self.items = []
+
+    def is_empty(self):
+        return (len(self.items) == 0)
+    
+
+    def add(self, item):
+        self.items.append(item)
+
+    def remove(self):
+        self.items.pop()
+
+    def check_line_length(self):
+        print(len(self.items))
+    
+
+
+myq = Queue()
+myq.add(55)
+myq.add(73)
+myq.add(22)
+
+print(myq.is_empty())
