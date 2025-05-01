d96f946ceb6f1883bd03d51104d2d6dd91be88fa
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-04-30 18:42:30 -0700

    update code

--- librarysort.py
+++ librarysort.py
@@ -2,3 +2,9 @@
 #works similarly to insertion sort but adds gaps to reduce the cost of shifting
 #elements 
 
+import math 
+
+def library_sort(arr, epsilon = 1):
+    if not arr:
+        return []
+    
